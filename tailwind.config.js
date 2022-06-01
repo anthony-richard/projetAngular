5554abc26701fc81a7f75b93035f9dca3cf75f41
Anthony RICHARD <anthonyrichard55200@email.com>
2022-06-01 11:29:45 +0200

    config tailwind + stylisation login/register/nav/footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary" : "#141E61",
+          "secondary" : "#0F044C",
+          "neutral" : "#191D24",
+          "base-100" : "#EEEEEE",
+          "info" : "#3ABFF8",
+          "success" : "#36D399",
+          "warning" : "#FBBD23",
+          "error" : "#F87272",
+        },
+      },
+    ],
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [
+    require('daisyui'),
+  ],
+}  
