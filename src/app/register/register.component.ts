37d66ddb66269bf4ba2ace745b08e6a7ddb7dce1
Anthony RICHARD <anthonyrichard55200@email.com>
2022-06-02 23:28:51 +0200

    inscription but bugged

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AuthentificationService } from '../service/authentification.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
-
+  registerForm : FormGroup
+  constructor(formBuilder : FormBuilder, private auth:AuthentificationService, private router:Router) {
+    this.registerForm = formBuilder.group({
+      email: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      pseudo: new FormControl("", [
+        Validators.required,
+      ]),
+      password: new FormControl("", [
+        Validators.required,
+      ]),
+      passwordConfirm: new FormControl("", [
+        Validators.required,
+      ])
+    })
+  }
+   
   ngOnInit(): void {
   }
 
+  submitForm() {
+    if (this.registerForm.valid) {
+      if (this.registerForm.value.password === this.registerForm.value.passwordConfirm){
+        this.auth.postSign(this.registerForm.value.email,this.registerForm.value.pseudo,this.registerForm.value.password);
+        this.router.navigateByUrl('');
+      } else {
+        alert('Mots de passe non identique');
+      }
+    } 
+  }
 }
--- src/app/service/authentification.service.ts
+++ src/app/service/authentification.service.ts
@@ -5,23 +5,34 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthentificationService {
-  private userData:{ token: string,id:string, email:string}
+  private userData:{ token: string,id:string, email:string};
+  private signData:{ token: string,id:string, email:string, password:string, pseudo:string};
   constructor(private http: HttpClient) {
     this.userData = {token:'',id:'',email:''}
+    this.signData = { token: '',id:'', email:'', password:'', pseudo:''}
   }
-
+  // connexion
   postLogin(email: string, password: string) {
     this.http.post<any>("https://reseau.jdedev.fr/api/user/connect",{"email":email,"password":password}).subscribe(data=>{
       this.userData=data;
-      return 'come on user connected';
+      return 'utilisateur connecté !';
     })
   }
+  // inscription
+  postSign(email: string, pseudo: string, password: string) {
+    this.http.post<any>("https://reseau.jdedev.fr/api/user",{"email":email,"password":password,"pseudo":pseudo}).subscribe(data=>{
+    this.signData=data;
+    return 'Eh mais vous vous êtes inscrit !';
+    })
+  }
+
   getUsersData() {
     return this.userData;
   }
   getJwt() {
     return this.userData.token;
   }
+  // deconnexion
   logout(){
     this.userData = {token:'',id:'',email:''};
   }
